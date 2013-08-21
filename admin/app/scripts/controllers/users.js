6d892de8db1246c778952a0f2e65bcdd11c4b203
Jan Antala <jan@antala.sk>
2013-08-21 16:34:20 +0200

    new user - modal window

--- admin/app/scripts/controllers/users.js
+++ admin/app/scripts/controllers/users.js
@@ -8,17 +8,17 @@ angular.module('adminApp')
     if ($scope.addUserForm.$valid) {
       Users.create($scope.user, function(user){
         $scope.users.push(user);
+        $scope.close();
       });
       $scope.user = {};
     }
   };
 
-  $scope.removeUser = function(user){
-    var index = $scope.users.indexOf(user);
-    if (index < 0) { return false; }
+  $scope.open = function() {
+    $scope.newUser = true;
+  };
 
-    Users.remove({'userId': $scope.users[index].userId}, function(){
-      $scope.users.splice(index, 1);
-    });
+  $scope.close = function() {
+    $scope.newUser = false;
   };
 });
