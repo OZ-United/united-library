01c3590c392f18259024daf6cba4e6aac9337a78
Jan Antala <jan@antala.sk>
2013-08-21 18:53:40 +0200

    return book from rents view

--- admin/app/scripts/controllers/rents.js
+++ admin/app/scripts/controllers/rents.js
@@ -1,8 +1,14 @@
 'use strict';
 
 angular.module('adminApp')
-  .controller('RentsCtrl', function ($scope, Rents) {
+  .controller('RentsCtrl', function ($scope, Rents, $location) {
   $scope.rents = Rents.query();
 
-
+  $scope.returnBook = function(rent){
+    Rents.returnBook({'rentId': rent.rentId}, function(res){
+      console.log(res);
+      rent.status = res.status;
+      rent.rent.returnDate = res.rent.returnDate;
+    });
+  };
 });
