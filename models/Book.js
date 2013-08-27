dff91608f07e02b3229f10eb364f728ccb90bbeb
Jan Antala <jan@antala.sk>
2013-08-27 15:52:30 +0200

    set Image from tmp

--- models/Book.js
+++ models/Book.js
@@ -26,6 +26,21 @@ BookModelSchema.virtual('bookId').get(function(){
   return this.id;
 });
 
+BookModelSchema.methods.setImage = function(cb){
+
+  var book = this;
+  var dir = path.join(process.cwd(), 'public');
+  var fs = require('fs-extra');
+
+  var tmp_path = book.cover;
+  var res_path = '/img/' + book.cover.split('/').pop();
+  book.cover = res_path;
+
+  fs.copy(path.join(dir, 'tmp', book.cover), path.join(dir, 'img', book.cover), function(err){
+    if (err) cb(err);
+    cb(undefined, book);
+  });
+};
 
 BookModelSchema.pre('save', function(next){
   if (this.isNew) {
@@ -35,6 +50,18 @@ BookModelSchema.pre('save', function(next){
     console.log(this.copies);
   }
   next();
+});
+
+BookModelSchema.pre('save', function(next){
+  if (this.isNew) {
+    this.setImage(function(err, book){
+      if (err) { return next(err); }
+      next();
+    });
+  }
+  else {
+    next();
+  }
 });
 
 BookModelSchema.pre('remove', function(next){
