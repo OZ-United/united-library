9567fc2a1e0673aa2ce9a698824d09b712986b23
Jan Antala <jan@antala.sk>
2013-10-06 13:52:37 +0200

    clear search query

--- admin/app/scripts/controllers/search.js
+++ admin/app/scripts/controllers/search.js
@@ -1,6 +1,8 @@
 'use strict';
 
 angular.module('adminApp')
-.controller('SearchCtrl', function ($scope, $routeParams, Search) {
+.controller('SearchCtrl', function ($scope, $routeParams, Search, $rootScope) {
   $scope.books = Search.query($routeParams);
+
+  $rootScope.query = '';
 });
