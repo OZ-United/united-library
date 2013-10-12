677848972ea6627a222874f37e3e15d71d08b7b1
Jan Antala <jan@antala.sk>
2013-10-12 23:12:52 +0200

    auth route

--- admin/app/scripts/controllers/main.js
+++ admin/app/scripts/controllers/main.js
@@ -1,11 +1,6 @@
 'use strict';
 
 angular.module('adminApp')
-.controller('MainCtrl', function ($scope, Auth, $location) {
-  $scope.auth = function(user) {
-    Auth.login(user).then(function(user){
-      console.log(user);
-      $location.path('/books');
-    });
-  };
+.controller('MainCtrl', function ($scope) {
+
 });
